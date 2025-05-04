503deb5128bf1125bc482838edebeccdda64e949
Christ Youzan <yanisarnold15@gmail.com>
2025-05-04 10:56:47 +0100

    dded js to contact formedited contact page to include placeholder
    Signed-off-by: Christ Youzan <yanisarnold15@gmail.com>

--- react-app/src/sections/Contact/Contact.tsx
+++ react-app/src/sections/Contact/Contact.tsx
@@ -3,21 +3,40 @@ import { useState } from "react";
 import "./Contact.css"
 import Banner from "../../components/commons/Banner/Banner";
 function Contact() {
+      // pulling user data from json file
       const personalDetails = personalData;
+      const [errors, setErrors] = useState({
+              name: "",
+            email: "",
+            message: "",
+      })
+     // React state to manage form inputs
       const [formData, setFormData] = useState({
             name: "",
             email: "",
             message: "",
       });
-      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      // handling changes 
+      const handleChange = (
+            
+            e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+           // Update the corresponding field in formData based on input's name
             setFormData({
                   ...formData,
                   [e.target.name]: e.target.value,
             });
       };
+      // handling submit 
       const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+            // Prevent page reload
             e.preventDefault();
+            // console.log object
             console.log(formData);
+            setFormData({
+                        name: "",
+            email: "",
+            message: "",
+            })
       };
       return (
             <div>
@@ -34,9 +53,9 @@ function Contact() {
                               <p>{personalDetails.phone}</p>
                         </div>
                         <form onSubmit={handleSubmit} className="formData">
-                              <input type="text" name="name" onChange={handleChange} placeholder="John Doe"/>
-                              <input type="email" name="email" onChange={handleChange} placeholder="JohnDoe1@gmail.com"/>
-                              {/* <textarea name="message" onChange={handleChange}></textarea> */}
+                              <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="John Doe" required/>
+                              <input type="email" name="email" onChange={handleChange} placeholder="JohnDoe1@gmail.com" value={formData.email} />
+                              <textarea name="message" onChange={handleChange} value={formData.message} ></textarea>
                               <button type="submit">Send</button>
                         </form>
                   </div>
