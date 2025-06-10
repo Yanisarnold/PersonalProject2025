c18578ede9dbbcb5e132a72cb07ddd3868f60752
christ <christ.youzan@myndup.com>
2025-06-10 18:37:31 +0100

    added errors to the contact field page

--- Fe/react-app/src/sections/Contact/Contact.tsx
+++ Fe/react-app/src/sections/Contact/Contact.tsx
@@ -21,15 +21,43 @@ function Contact() {
       };
       // handling submit 
       const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-            // Prevent page reload
+                 // Prevent page reload
             e.preventDefault();
-            // console.log object
+            let newErrors = { name: "", email: "", message: "", };
+            let isValid = true;
+            
+            if (!formData.name) {
+                  newErrors.name = "Name is required.";
+                  isValid = false;
+            }
+
+            if (!formData.email) {
+                  newErrors.email = "Email is required.";
+                  isValid = false;
+                  // 
+            } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+                  newErrors.email = "Email is invalid.";
+                  isValid = false;
+            }
+
+            if (!formData.message) {
+                  newErrors.message = "Message is required.";
+                  isValid = false;
+            }
+
+            if (formData.message && formData.message.length < 10) {
+                  newErrors.message = "Message must be at least 10 characters.";
+                  isValid = false;
+            }
+
+            setErrors(newErrors);
+            if (isValid) {
             console.log(formData);
-            setFormData({name: "", email: "",message: "",})
+            setFormData({ name: "", email: "", message: "", })     
+            }
       };
       return (
             <div>
-                  {/* <Banner pageTitle="Christ Youzan" /> */}
                   <div className="container">
                         <div className="contactBanner">
 
@@ -42,9 +70,19 @@ function Contact() {
                               <p>{personalDetails.phone}</p>
                         </div>
                         <form onSubmit={handleSubmit} className="formData">
-                              <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="John Doe" required/>
-                              <input type="email" name="email" onChange={handleChange} placeholder="JohnDoe1@gmail.com" value={formData.email} />
+                              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                              <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="John Doe"  />
+                                    {errors.name && <span className="error" style={{ color: 'red' }}>{errors.name}</span>}
+                              </div>
+                             
+                                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                    <input type="email" name="email" onChange={handleChange} placeholder="JohnDoe1@gmail.com" value={formData.email} />
+                                    {errors.email && <span className="error" style={{ color: 'red' }}>{errors.email}</span>}
+                              </div>
+                              <div style={{ display: 'flex', flexDirection: 'column' }}>
                               <textarea name="message" onChange={handleChange} value={formData.message} ></textarea>
+                                {errors.message && <span className="error" style={{ color: 'red' }}>{errors.message}</span>}
+                              </div>
                               <button type="submit">Send</button>
                         </form>
                   </div>
