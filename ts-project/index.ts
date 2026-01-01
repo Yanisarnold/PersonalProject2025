18091c09e1e8e8b73d28da83716cd7d0d5469a55
Christ Y <christ.youzan@kaluza.com>
2026-01-01 19:33:54 +0000

    added different type + interface

--- ts-project/index.ts
+++ ts-project/index.ts
@@ -1,2 +1,20 @@
 const firstName = "williams"; 
 const lastName = "Tim";
+const age = 24;
+const isEmployed = false;
+const arrayOfCars = ["Mercedes", "BMW", "Ferrari", "Lamborghini"];
+const ObjectOfFood = {food_One:"Pie", food_Two: "Burger"}
+
+
+type food = {
+    name:string, 
+    color?:string,
+    price:string
+}
+
+interface countryOfOrigin  {
+    country: string;
+    city: string,
+    flight: number
+
+}
