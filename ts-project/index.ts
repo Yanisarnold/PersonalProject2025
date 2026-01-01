9aa6cb6a005bce017f8eba036efd90f875974320
Christ Y <christ.youzan@kaluza.com>
2026-01-01 19:36:28 +0000

    added comments to everything defined

--- ts-project/index.ts
+++ ts-project/index.ts
@@ -1,20 +1,26 @@
+// different types
 const firstName = "williams"; 
 const lastName = "Tim";
 const age = 24;
 const isEmployed = false;
+// array + object definition 
 const arrayOfCars = ["Mercedes", "BMW", "Ferrari", "Lamborghini"];
 const ObjectOfFood = {food_One:"Pie", food_Two: "Burger"}
 
-
+// type definition 
 type food = {
     name:string, 
     color?:string,
     price:string
 }
-
+// interface
 interface countryOfOrigin  {
     country: string;
     city: string,
     flight: number
 
 }
+
+
+
+// todo app 
