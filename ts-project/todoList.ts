7e6f30d88b287a659ecd1b00b14993fead3f933a
Christ Y <christ.youzan@kaluza.com>
2026-01-05 10:00:17 +0000

    added dates and the ability to mark task as complete

--- ts-project/todoList.ts
+++ ts-project/todoList.ts
@@ -1,9 +1,31 @@
 // add, remove, list, modify,savetoJson (task)
+
 import * as fs from "fs";
 
-type Task = [
-   string, UrgencyOfTask
-]
+const uuid = Math.floor(Math.random() * 1000);
+const isCompleted = false;
+const taskCreatedAt = new Date().toISOString().split("T")[0];
+ console.log(taskCreatedAt);
+const expiryDate = "";
+console.log(uuid);
+// type of task the array can store
+type Task = {
+ id: number,
+ name:string, 
+ urgency: UrgencyOfTask,
+ isCompleted: boolean,
+ taskCreatedAt: string
+}
+
+export type UrgencyOfTask =
+  typeof urgencyOfTask[keyof typeof urgencyOfTask];
+export const  urgencyOfTask = {
+
+    Low :"Low",
+    Medium : "Medium",
+    Urgent : "Urgent"
+} as const;
+
 const storeTask: Task[] = [];
 
 
@@ -11,17 +33,17 @@ const addTask = (task : string,  taskUrgency: UrgencyOfTask ) => {
     if(typeof(task) !== "string"){
         throw new Error("Invalid type added " + typeof(task) )
     }
- storeTask.push([task, taskUrgency]);
+ storeTask.push({id: uuid,name: task,urgency: taskUrgency, isCompleted, taskCreatedAt});
  console.log(`task added:  ${storeTask.length}`)
 
 for (const task of storeTask) {
-  console.log(`current task added: ${task[0]}, Urgency: (${task[1]})`);
+  console.log(`current task added: ${task.id} ${task.name}, Urgency: (${task.urgency}), Completed?: ${task.isCompleted}, CreatedAt: ${task.taskCreatedAt}`);
 }
 }
-
-const removeTask = (task: string) => {
+// const index = storeTask.findIndex(task => task.id === id);
+const removeTask = (uuid: number) => {
       const index = storeTask.findIndex(
-    ([taskName]) => taskName === task
+    (task) => task.id === uuid
   );
     if (index !== -1) {
         storeTask.splice(index,1);
@@ -32,24 +54,26 @@ const removeTask = (task: string) => {
 function printAllTask() {
   console.log("Current tasks:");
 
-  for (const [index, [task, urgency]] of storeTask.entries()) {
-    console.log(`${index + 1}. ${task}, Urgency: ${urgency}`);
+//   iterate over an array with multiple input
+  for (const [index, task] of storeTask.entries()) {
+    console.log(`${index + 1}. Id: ${task.id} , Name: ${task.name}, Urgency: ${task.urgency}, Completed?: ${task.isCompleted}, CreatedAt: ${task.taskCreatedAt}`);
   }
 
   console.log(`Total tasks: ${storeTask.length}`);
 }
 
 
-// function modifyTask(currTask:string, NewTask:string ) {
-//     // checking currTask index 
-//  const index = storeTask.indexOf(currTask.toLowerCase())
-// //  checking if it exist then changing to new value + returning true 
-//  if (index !== -1){
-//     storeTask[index] = NewTask.toLowerCase();
-//     return true;
-//  }
-// return false
-// }
+function modifyTask(currTask:string, NewTask:string ) {
+    // checking currTask index 
+ const index = storeTask.findIndex(
+    (taskName) => taskName.name === currTask)
+//  checking if it exist then changing to new value + returning true 
+ if (index !== -1){
+    storeTask[index][0] = NewTask.toLowerCase();
+    return true;
+ }
+return false
+}
 
 
 
@@ -61,21 +85,13 @@ fs.writeFile(outputFilePath, JSON.stringify(storeTask, null, 4), 'utf8', () => {
   console.log(`Data written to ${outputFilePath} as JSON.`);
 });
 }
-export const  urgencyOfTask = {
-    Low :"Low",
-    Medium : "Medium",
-    Urgent : "Urgent"
-} as const;
 
-
-export type UrgencyOfTask =
-  typeof urgencyOfTask[keyof typeof urgencyOfTask];
 // testing
 addTask("food", urgencyOfTask.Medium)
-addTask("ronaldo", urgencyOfTask.Medium)
+addTask("ronaldo", urgencyOfTask.Urgent)
 printAllTask()
-// modifyTask("Ronaldo", "r9")
-removeTask("food")
+modifyTask("ronaldo", "r9")
+removeTask(uuid)
 printAllTask()
 savetoJson()
 
